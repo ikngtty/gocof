821751dd8ff6a45e8ad95b539478806fe96d6618
ikngtty <ikngtty@gmail.com>
2022-08-03 22:17:16 +0900

    prototype

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ikngtty/gocof
 
 go 1.18
+
+require golang.org/x/tools v0.1.12
--- main.go
+++ main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
+	"io/fs"
+	"os"
+	"strings"
+
+	"golang.org/x/tools/go/ast/astutil"
+)
 
 func main() {
-	fmt.Println("hello world")
+	srcFset := token.NewFileSet()
+	const srcPath = "/Users/ikngtty/Projects/src/github.com/ikngtty/go-contestlib/math/modular"
+	srcPkgs, err := parser.ParseDir(srcFset, srcPath, func(fi fs.FileInfo) bool {
+		return !strings.HasSuffix(fi.Name(), "_test.go")
+	}, 0)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	const dstPath = "/Users/ikngtty/Projects/src/github.com/ikngtty/contest-template/AtCoder/Golang/main.go"
+	dstFileIO, err := os.OpenFile(dstPath, os.O_APPEND|os.O_WRONLY, 0)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer dstFileIO.Close()
+
+	for _, srcPkg := range srcPkgs {
+		for _, srcFile := range srcPkg.Files {
+			astutil.Apply(srcFile, nil, func(cur *astutil.Cursor) bool {
+				decl, ok := cur.Node().(*ast.GenDecl)
+				if !ok {
+					return true
+				}
+				if decl.Tok == token.IMPORT {
+					cur.Delete()
+				}
+				return true
+			})
+
+			err := format.Node(dstFileIO, srcFset, srcFile.Decls)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			_, err = dstFileIO.Write([]byte("\n\n"))
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+	}
 }
